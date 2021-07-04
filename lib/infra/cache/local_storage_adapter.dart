2ccf049696974ed273ac468e5073d03f694c8141
Kevin Kobori <kevinyujikobori@gmail.com>
2021-07-03 22:49:06 -0300

    LocalStorageAdapter implements SaveSecureCachedStorage

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -1,7 +1,8 @@
+import 'package:for_dev/data/cache/cache.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class LocalStorageAdapter {
+class LocalStorageAdapter implements SaveSecureCachedStorage {
   final FlutterSecureStorage secureStorage;
   LocalStorageAdapter({
     @required this.secureStorage,
